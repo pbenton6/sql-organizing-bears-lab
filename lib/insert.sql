f43b9c6153597c77d55ed8e9aac7fa62689413ff
pbenton6 <patrick.benton6396@gmail.com>
2021-11-15 18:30:46 -0600

    commit tree 7ce4b030a0bbdd6eda7872c8565063575d1415d0

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES(1,'Mr. Chocolate', 18, 'F', 'green','kind', 1);
+INSERT INTO bears VALUES(2, 'Rowdy', 15, 'M', 'grey', 'rowdy', 1);
+INSERT INTO bears VALUES(3, 'Tabitha', 8, 'F', 'brown', 'sassy', 1);
+INSERT INTO bears VALUES(4, 'Sergeant Brown', 6, 'M', 'brown', 'militaristic', 1);
+INSERT INTO bears VALUES(5, 'Melissa', 4, 'F', 'black', 'sarcastic', 1);
+INSERT INTO bears VALUES(6, 'Grinch', 12, 'F', 'white', 'mean', 1);
+INSERT INTO bears VALUES(7, 'Wendy', 9, 'F', 'golden', 'nice', 1);
+INSERT INTO bears VALUES(8, null, 10, 'M', 'grey', 'werid' , 1);
